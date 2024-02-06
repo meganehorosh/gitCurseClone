986c4baa61fceeb6d98c76ed488b1cccfafc2308
meganehorosh <meganehorosh@gmail.com>
2024-02-06 16:02:17 +0300

    1

--- clone.py
+++ clone.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import json
 
 x = os.listdir("S:/Games/minecraft/Instances/")
 
@@ -7,3 +8,8 @@ for i in x:
     if not os.path.exists("S:/Games/gitCurseClone/"+i):
         os.mkdir("S:/Games/gitCurseClone/"+i)
     shutil.copy("S:/Games/minecraft/Instances/"+i+"/minecraftinstance.json","S:/Games/gitCurseClone/"+i+"/minecraftinstance.json")
+
+data = {'names': x}
+
+with open('list.json', 'w') as f:
+    json.dump(data, f)
